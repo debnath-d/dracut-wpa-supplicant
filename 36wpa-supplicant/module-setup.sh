8f8283f6fce5949b7e65c9d69814a37474ad4d58
D. Debnath <d_debnath@outlook.com>
2024-05-17 00:04:04 +0530

    Initial commit

---
+++ 36wpa-supplicant/module-setup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# 2024, D. Debnath
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+# called by dracut
+check() {
+    require_binaries wpa_supplicant || return 1
+    # 0 enables by default, 255 only on request
+    return 0
+}
+
+# called by dracut
+depends() {
+    echo dbus bash net-lib
+    return 0
+}
+
+# called by dracut
+install() {
+    inst wpa_supplicant
+
+    inst /usr/lib/systemd/system/wpa_supplicant.service
+    inst /usr/share/dbus-1/system-services/fi.w1.wpa_supplicant1.service
+    inst /usr/share/dbus-1/system.d/wpa_supplicant.conf
+
+    $SYSTEMCTL -q --root "$initdir" enable wpa_supplicant.service
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+This [Dracut][dracut] module (`dracut-wpa-supplicant`) integrates 
+[`wpa_supplicant`](https://w1.fi/wpa_supplicant/) into the [initramfs][iramfs].
+
+It's compatible with systems that use Dracut as initramfs manager
+and systemd as init system.
+
+2024, D. Debnath, GPLv3+
+
+[dracut]: https://dracut.wiki.kernel.org/index.php/Main_Page
+[iramfs]: https://en.wikipedia.org/wiki/Initial_ramdisk
